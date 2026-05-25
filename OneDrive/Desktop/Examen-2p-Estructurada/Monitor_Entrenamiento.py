df7b7fc93d8d634f7f9a28bb7b43c9c4fd4bd80f
Héctor <hectorgarcia1893@hotmail.com>
2026-05-25 07:58:11 -0600

    Prueba de la primer funcion dentro del archivo Monitor_entrenamiento

---
+++ OneDrive/Desktop/Examen-2p-Estructurada/Monitor_Entrenamiento.py
@@ -0,0 +1,35 @@
+"""
+Héctor Luis Garcia Marthen
+Ux25II175
+25/05/2026
+Examen Segundo Parcial - Programación Estructurada
+"""
+
+
+# Importación de bibliotecas estandar
+
+import datetime
+import math
+import random
+import statistics
+import sys
+
+MAX_EPOCHS = 10
+UMBRAL_ERROR_CRITICO = 0.95
+
+
+def obtener_info_sistema():
+    print("Información del sistema:")
+    print(f"Plataforma: {sys.platform}")
+    print(f"Versión de Python: {sys.version}")
+    print(f"salida limpia: {sys.exit(1)}")
+
+
+
+def main():
+    obtener_info_sistema()
+
+
+if __name__ == "__main__":
+    main()
+    
